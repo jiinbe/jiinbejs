e6aca4f174ddf85eddc184b445eca3b6d4eea768
Ferry Ayunda <165256908+jiinbe@users.noreply.github.com>
2024-07-26 14:29:06 +0700

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -55,6 +55,9 @@ statusElement.textContent = config.messages.uploading;
 let uploadedImage = document.createElement('img');
 uploadedImage.setAttribute("width", "100");
 uploadedImage.setAttribute("height", "100");
+uploadedImage.style.position = 'absolute';
+uploadedImage.style.top = 100;
+uploadedImage.style.left = 60;
 uploadedImage.src = URL.createObjectURL(this.files[0]);
 			
 uploadedImage.addEventListener('load', function() {
